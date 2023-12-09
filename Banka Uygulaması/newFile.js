2fa10e8b0538c184d4556934294b874392b8fbfe
Ömer Köse <117233659+omerkse@users.noreply.github.com>
2023-12-09 13:16:09 +0300

    Add files via upload

---
+++ Banka Uygulaması/newFile.js
@@ -0,0 +1,14 @@
+"use strict";
+btnClose.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      (acc) => acc.username === currentAccount.username,
+      (labelWelcome.textContent = "Log in to get started")
+    );
+  }
+});
